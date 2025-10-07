c6b770d3d54f355f3512896dd2f288c6122c720d
FranciscoMunzon <francisco.munzon@a.vedrunasevillasj.es>
2025-10-07 20:25:27 +0200

    Segundo Commit

---
+++ Ejercicio/Boletin-02.js
@@ -0,0 +1,41 @@
+//1.Cambiar el color de un párrafo al hacer clic en un botón.
+
+function cambiarColor() {
+    const parrafo = document.getElementById("parrafoColor");
+    parrafo.style.color = parrafo.style.color === "blue" ? "black" : "blue";
+}
+
+//2.Crear un párrafo nuevo dentro de un div.
+
+function crearParrafo() {
+    const div = document.getElementById("contenedorParrafos");
+    const nuevoParrafo = document.createElement("parrafo");
+    nuevoParrafo.textContent = "Hola, soy nuevo párrafo";
+    div.appendChild(nuevoParrafo);
+}
+
+//3.Eliminar el último elemento de una lista con un botón.
+
+function eliminarUltimo() {
+    const lista = document.getElementById("Lista");
+    lista.removeChild(lista.lastElementChild);
+}
+
+//4.Cambiar el texto de un título (h1) cuando el ratón pase por encima.
+
+function cambiarTexto() {
+    const titulo = document.getElementById("tituloRaton");
+    titulo.textContent = "¡Enhorabuena! Has ganado absolutamente nada";
+}
+
+function restaurarTexto() {
+    const titulo = document.getElementById("tituloRaton");
+    titulo.textContent = "Pasa el ratón por aquí";
+}
+
+//5.Ocultar y mostrar un div con un botón.
+
+function toggleDiv() {
+    const div = document.getElementById("divOculto");
+    div.style.display = (div.style.display === "none") ? "block" : "none";
+}
